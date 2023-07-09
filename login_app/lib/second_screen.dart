12d7ff2631f695be4187fc2eb7217f01c24a30b0
Matheus <plutaozaum@gmail.com>
2023-07-09 14:53:40 -0300

    style: add a bottom navigation in second screen

--- login_app/lib/second_screen.dart
+++ login_app/lib/second_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_function_type_syntax_for_parameters, unused_import
+// ignore_for_file: prefer_const_constructors, use_function_type_syntax_for_parameters, unused_import, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:my_app/login_screen.dart';
@@ -9,23 +9,23 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Bem vindo ao meu APP!",
-              style: TextStyle(fontSize: 20),
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.arrow_back),
-            )
-          ],
-        ),
+      appBar: AppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu),
+            label: "Menu",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
       ),
     );
   }
